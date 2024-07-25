8c6554800cec8700c1a839ab4a16ce85138fadfc
denislav-mladenov <denislav.mladenov.89@gmail.com>
2024-07-25 22:04:05 +0300

    config change

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,11 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   networks: {
     devnet: {
-      url: "http://localhost:8545",
-    },
+      url: "http://127.0.0.1:8545",
+      chainId: 1337,
+      gas: 8000000,
+      loggingEnabled: true // Enable logging for debugging
+    }
   },
+  mocha: {
+    timeout: 200000 // Increase timeout for network interactions
+  }
 };
+
