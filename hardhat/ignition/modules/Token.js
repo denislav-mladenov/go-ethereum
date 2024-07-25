998feb72d76ac09856b5d385e0ea88a8d0ba6f31
denislav-mladenov <denislav.mladenov.89@gmail.com>
2024-07-25 21:05:45 +0300

    Token update

--- hardhat/ignition/modules/Token.js
+++ hardhat/ignition/modules/Token.js
@@ -1,9 +1,15 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const TokenModule = buildModule("TokenModule", (m) => {
-  const token = m.contract("Token");
+const JAN_1ST_2030 = 1893456000;
+const ONE_GWEI = 1_000_000_000n;
 
-  return { token };
+module.exports = buildModule("LockModule", (m) => {
+  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
+  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+
+  const lock = m.contract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  return { lock };
 });
-
-module.exports = TokenModule;
